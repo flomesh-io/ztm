420268b17325f30ed229de4dda57310d7d5bf856
pajama-coder <pajamacoder@flomesh.io>
2024-04-24 12:36:28 +0800

    Revised the API of the CA service

--- ca/db.js
+++ ca/db.js
@@ -3,63 +3,79 @@ var db = null
 function open(pathname) {
   db = sqlite(pathname)
 
-  if (db.exec(
-    `SELECT * FROM sqlite_schema WHERE type = 'table' AND name = 'users'`
-  ).length === 0) {
-    db.exec(`
-      CREATE TABLE users (
-        name TEXT PRIMARY KEY,
-        cert TEXT NOT NULL,
-        key TEXT NOT NULL,
-        roles TEXT NOT NULL
-      )
-    `)
-  }
+  db.exec(`
+    CREATE TABLE IF NOT EXISTS certificates (
+      name TEXT PRIMARY KEY,
+      data TEXT NOT NULL
+    )
+  `)
+
+  db.exec(`
+    CREATE TABLE IF NOT EXISTS keys (
+      name TEXT PRIMARY KEY,
+      data TEXT NOT NULL
+    )
+  `)
 }
 
-function getUser(name) {
-  return db.sql(
-    `SELECT name, cert, key, roles from users WHERE name = ?`
-  )
-  .bind(1, name)
-  .exec()[0]
-}
-
-function setUser(name, obj) {
-  var old = getUser(name)
-  if (old) {
-    Object.assign(old, obj)
-    db.sql(
-      `UPDATE users SET cert = ?, key = ?, roles = ? WHERE name = ?`
-    )
-    .bind(1, obj.cert)
-    .bind(2, obj.key || '')
-    .bind(3, JSON.stringify(obj.roles || {}))
-    .bind(4, name)
-    .exec()
-  } else {
-    db.sql(
-      `INSERT INTO users(name, cert, key, roles) VALUES(?, ?, ?, ?)`
-    )
+function getCert(name) {
+  return db.sql(`SELECT data FROM certificates WHERE name = ?`)
     .bind(1, name)
-    .bind(2, obj.cert)
-    .bind(3, obj.key || '')
-    .bind(4, JSON.stringify(obj.roles || {}))
-    .exec()
+    .exec()[0]?.data
+}
+
+function setCert(name, data) {
+  if (getKey(name)) {
+    db.sql(`UPDATE certificates SET data = ? WHERE name = ?`)
+      .bind(1, data)
+      .bind(2, name)
+      .exec()
+  } else {
+    db.sql(`INSERT INTO certificates(name, data) VALUES(?, ?)`)
+      .bind(1, name)
+      .bind(2, data)
+      .exec()
   }
 }
 
-function delUser(name) {
-  db.sql(
-    `DELETE FROM users WHERE name = ?`
-  )
-  .bind(1, name)
-  .exec()
+function delCert(name) {
+  db.sql(`DELETE FROM certificates WHERE name = ?`)
+    .bind(1, name)
+    .exec()
+}
+
+function getKey(name) {
+  return db.sql(`SELECT data FROM keys WHERE name = ?`)
+    .bind(1, name)
+    .exec()[0]?.data
+}
+
+function setKey(name, data) {
+  if (getKey(name)) {
+    db.sql(`UPDATE keys SET data = ? WHERE name = ?`)
+      .bind(1, data)
+      .bind(2, name)
+      .exec()
+  } else {
+    db.sql(`INSERT INTO keys(name, data) VALUES(?, ?)`)
+      .bind(1, name)
+      .bind(2, data)
+      .exec()
+  }
+}
+
+function delKey(name) {
+  db.sql(`DELETE FROM keys WHERE name = ?`)
+    .bind(1, name)
+    .exec()
 }
 
 export default {
   open,
-  getUser,
-  setUser,
-  delUser,
+  getCert,
+  setCert,
+  delCert,
+  getKey,
+  setKey,
+  delKey,
 }
