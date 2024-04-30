09a743de70914f8ee714b1b864b484d4464ad8f8
DongChen Lin <393227948@qq.com>
2024-04-30 09:09:45 +0800

    fix gui issue

--- gui/src/views/mesh/Log.vue
+++ gui/src/views/mesh/Log.vue
@@ -90,7 +90,7 @@ const timeago = computed(() => (ts) => {
 					innerClass="transparent" 
 					@load="load" 
 					@select="select"/>
-				<Textarea @keyup="watchEnter" v-model="typing" :autoResize="true" class="drak-input bg-gray-900 text-white" placeholder="Type service | port" rows="1" cols="30" />
+				<Textarea @keyup="watchEnter" v-model="typing" :autoResize="true" class="drak-input bg-gray-900 text-white" placeholder="Type keywork" rows="1" cols="30" />
 				<Button :disabled="!typing" icon="pi pi-search"  @click="clickSearch"/>
 			</InputGroup>
 		</template>
