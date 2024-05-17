04bb0c506f560c66ef334c446728f79dd0def978
pajama-coder <pajamacoder@flomesh.io>
2024-05-17 10:22:06 +0800

    Added help info to the CLI

---
+++ cli/help.js
@@ -0,0 +1,243 @@
+export default function (argv) {
+  switch (argv[0]) {
+    case 'config': return helpConfig()
+    case 'run':
+      switch (argv[1]) {
+        case 'ca': return helpRunCA()
+        case 'hub': return helpRunHub()
+        case 'agent': return helpRunAgent()
+        default: return helpRun()
+      }
+    case 'invite': return helpInvite()
+    case 'evict': return helpEvict()
+    case 'join': return helpJoin()
+    case 'leave': return helpLeave()
+    case 'get': return helpGet()
+    case 'describe': return helpDescribe()
+    case 'create':
+      switch (argv[1]) {
+        case 'service': return helpCreateService()
+        case 'port': return helpCreatePort()
+        default: return helpCreate()
+      }
+    case 'delete': return helpDelete()
+    default: return helpAll()
+  }
+}
+
+function helpAll() {
+  println()
+  println(`Usage: ztm <command> [<object type>] [<object name>] [<options>]`)
+  println()
+  println(`Commands:`)
+  println()
+  println(`  config`)
+  println(`  start     ca | hub | agent`)
+  println(`  stop      ca | hub | agent`)
+  println(`  run       ca | hub | agent`)
+  println(`  invite`)
+  println(`  evict`)
+  println(`  join`)
+  println(`  leave`)
+  println(`  get       service | port | endpoint | mesh`)
+  println(`  describe  service | port | endpoint | mesh`)
+  println(`  create    service | port`)
+  println(`  delete    service | port`)
+  println()
+  println(`Object types:`)
+  println()
+  println(`  ca`)
+  println(`  hub`)
+  println(`  agent`)
+  println(`  mesh      meshes`)
+  println(`  endpoint  endpoints  ep`)
+  println(`  service   services   svc`)
+  println(`  port      ports`)
+  println()
+  helpServiceName()
+  println()
+  helpPortName()
+  println()
+  println(`Type 'ztm help <command> [<object name>]' for more details.`)
+  println()
+}
+
+function helpServiceName() {
+  println(`Service name:`)
+  println()
+  println(`  tcp/name`)
+  println(`  udp/name`)
+  println()
+  println(`  e.g. 'tcp/my-fancy-service', 'udp/voice-chat-svc'`)
+}
+
+function helpPortName() {
+  println(`Port name:`)
+  println()
+  println(`  ip/tcp/port`)
+  println(`  ip/udp/port`)
+  println(`  localhost/tcp/port`)
+  println(`  localhost/udp/port`)
+  println(`  tcp/port`)
+  println(`  udp/port`)
+  println()
+  println(`  e.g. '10.0.0.2/tcp/8080', 'localhost/udp/445', 'tcp/80'`)
+}
+
+function helpConfig() {
+  println()
+  println(`Usage: ztm config [--ca=<host>:<port>] [--agent=<host>:<port>]`)
+  println()
+  println(`Options:`)
+  println(`  --ca=<host>:<port>      Where the CA service can be accessed`)
+  prinlnt(`                          Defaults to 'localhost:9999'`)
+  println(`  --agent=<host>:<port>   Where the agent service can be accessed`)
+  println(`                          Defaults to 'localhost:7777'`)
+  println()
+}
+
+function helpRun() {
+  println()
+  println(`Usage: ztm run ca|hub|agent <options>`)
+  println()
+  println(`Type 'ztm help run ca|hub|agent' for more details.`)
+  println()
+}
+
+function helpRunCA() {
+  println()
+  println(`Usage: ztm run ca [--listen=[<ip>:]<port>] [--database=<filename>]`)
+  println()
+  println(`Options:`)
+  println(`  -l, --listen=[<ip>:]<port>   Service listening address`)
+  println(`                               Defaults to '127.0.0.1:9999'`)
+  println(`  -d, --database=<filename>    Location of the database file`)
+  println(`                               Defaults to '~/ztm-ca.db'`)
+  println()
+}
+
+function helpRunHub() {
+  println()
+  println(`Usage: ztm run hub [--listen=[<ip>:]<port>] [--ca=<host>:<port>] [--name=<address>]`)
+  println()
+  println(`Options:`)
+  println(`  -l, --listen=[<ip>:]<port>   Service listening address`)
+  println(`                               Defaults to '127.0.0.1:8888'`)
+  println(`      --ca=<host>:<port>       Address of the CA service to connect to`)
+  println(`                               Defaults to 'localhost:9999'`)
+  println(`  -n, --name=<address>         Address of the hub that is visible to agents`)
+  println(`                               More than one addresses can be provided if any`)
+  println()
+}
+
+function helpRunAgent() {
+  println()
+  println(`Usage: ztm run agent [--listen=[<ip>:]<port>] [--database=<filename>]`)
+  println()
+  println(`Options:`)
+  println(`  -l, --listen=[<ip>:]<port>   Service listening address`)
+  println(`                               Defaults to '127.0.0.1:7777'`)
+  println(`  -d, --database=<filename>    Location of the database file`)
+  println(`                               Defaults to '~/ztm.db'`)
+  println()
+}
+
+function helpInvite() {
+  println()
+  println(`Usage: ztm invite <username> --bootstrap=<host>:<port> [--output=<filename>]`)
+  println()
+  println(`Options:`)
+  println(`  -b, --bootstrap=<host>:<port>   Target address of a hub for endpoint agents to connect to`)
+  println(`  -o, --output=<filename>         Filename of the generated permit for an endpoint to join the mesh`)
+  println(`                                  Output to stdout if not present`)
+  println()
+}
+
+function helpEvict() {
+  println()
+  println(`Usage: ztm evict <username>`)
+  println()
+}
+
+function helpJoin() {
+  println()
+  println(`Usage: ztm join <mesh name> --as=<name> --permit=<filename>`)
+  println()
+  println(`Options:`)
+  println(`  -a, --as      Specify a name of the endpoint seen by other endpoints in the mesh`)
+  println(`  -p, --permit  Point to a file containing the permit for joining the mesh`)
+  println(`                A permit is generated with command 'ztm invite'`)
+  println()
+}
+
+function helpLeave() {
+  println()
+  println(`Usage: ztm leave <mesh name>`)
+  println()
+}
+
+function helpGet() {
+  println()
+  println(`Usage: ztm get service|port|endpoint|mesh [<name>]`)
+  println()
+  helpServiceName()
+  println()
+  helpPortName()
+  println()
+}
+
+function helpDescribe() {
+  println()
+  println(`Usage: ztm describe service|port|endpoint|mesh <name>`)
+  println()
+  helpServiceName()
+  println()
+  helpPortName()
+  println()
+}
+
+function helpCreate() {
+  println()
+  println(`Usage: ztm create service|port <name> <options>`)
+  println()
+  helpServiceName()
+  println()
+  helpPortName()
+  println()
+  println(`Type 'ztm help create service|port' for more details.`)
+  println()
+}
+
+function helpCreateService() {
+  println()
+  println(`Usage: ztm create service <name> --host=<host> --port=<port>`)
+  println()
+  println(`Options:`)
+  println(`  -h, --host  Host name or IP address of the service`)
+  println(`  -p, --port  Port number of the service`)
+  println()
+  helpServiceName()
+  println()
+}
+
+function helpCreatePort() {
+  println()
+  println(`Usage: ztm create service <name> --service=<name> [--endpoint=<name>]`)
+  println()
+  println(`Options:`)
+  println(`  -s, --service         Name of the service that the port connects to`)
+  println(`  -e, --ep, --endpoint  Optional name of the endpoint that provides the service`)
+  println()
+  helpPortName()
+  println()
+}
+
+function helpDelete() {
+  println()
+  println(`Usage: ztm delete service|port [<name>]`)
+  println()
+  helpServiceName()
+  println()
+  helpPortName()
+  println()
+}
--- cli/main.js
+++ cli/main.js
@@ -2,63 +2,149 @@
 
 import options from './options.js'
 import clients from './clients.js'
+import help from './help.js'
 
-function help() {
-  println(`Usage: ztm <command> [<object type>] [<object name>] [<options>]`)
-  println()
-  println(`Commands:`)
-  println()
-  println(`  config`)
-  println(`  start     ca | hub | agent`)
-  println(`  stop      ca | hub | agent`)
-  println(`  run       ca | hub | agent`)
-  println(`  invite`)
-  println(`  evict`)
-  println(`  join`)
-  println(`  leave`)
-  println(`  get       service | port | endpoint | mesh`)
-  println(`  describe  service | port | endpoint | mesh`)
-  println(`  create    service | port`)
-  println(`  delete    service | port`)
-  println()
-  println(`Object types:`)
-  println()
-  println(`  ca`)
-  println(`  hub`)
-  println(`  agent`)
-  println(`  mesh      meshes`)
-  println(`  endpoint  endpoints  ep`)
-  println(`  service   services   svc`)
-  println(`  port      ports`)
-  println()
-  println(`Service name:`)
-  println()
-  println(`  tcp/name`)
-  println(`  udp/name`)
-  println()
-  println(`  e.g. 'tcp/my-fancy-service', 'udp/voice-chat-svc'`)
-  println()
-  println(`Port name:`)
-  println()
-  println(`  ip/tcp/port`)
-  println(`  ip/udp/port`)
-  println(`  localhost/tcp/port`)
-  println(`  localhost/udp/port`)
-  println(`  tcp/port`)
-  println(`  udp/port`)
-  println()
-  println(`  e.g. '10.0.0.2/tcp/8080', 'localhost/udp/445', 'tcp/80'`)
-  println()
-  println(`Type 'ztm help <command> [<object name>]' for more details.`)
-  println()
+//
+// Options
+//
+
+var optionsConfig = {
+  defaults: {
+    '--ca': '',
+    '--agent': '',
+  },
+  shorthands: {},
 }
 
-var InputError = (msg, command) => `${msg}. Type 'ztm ${command ? 'help ' + command : 'help'}' for help.`
-var InvalidObjectType = (type, command) => InputError(`invalid object type '${type}' for command '${command}'`, command)
+var optionsCA = {
+  defaults: {
+    '--database': '~/ztm-ca.db',
+    '--listen': '0.0.0.0:9999',
+  },
+  shorthands: {
+    '-d': '--database',
+    '-l': '--listen',
+  },
+}
+
+var optionsHub = {
+  defaults: {
+    '--ca': 'localhost:9999',
+    '--listen': '0.0.0.0:8888',
+    '--name': [],
+  },
+  shorthands: {
+    '-l': '--listen',
+    '-n': '--name',
+  },
+}
+
+var optionsAgent = {
+  defaults: {
+    '--database': '~/ztm.db',
+    '--listen': '127.0.0.1:7777',
+  },
+  shorthands: {
+    '-d': '--database',
+    '-l': '--listen',
+  },
+}
+
+var optionsInvite = {
+  defaults: {
+    '--bootstrap': [],
+    '--output': '',
+  },
+  shorthands: {
+    '-b': '--bootstrap',
+    '-o': '--output',
+  },
+}
+
+var optionsJoin = {
+  defaults: {
+    '--as': '',
+    '--permit': '',
+  },
+  shorthands: {
+    '-a': '--as',
+    '-p': '--permit',
+  },
+}
+
+var optionsService = {
+  defaults: {
+    '--host': '',
+    '--port': 0,
+  },
+  shorthands: {
+    '-h': '--host',
+    '-p': '--port',
+  },
+}
+
+var optionsPort = {
+  defaults: {
+    '--service': '',
+    '--endpoint': '',
+  },
+  shorthands: {
+    '-s': '--service',
+    '-e': '--endpoint',
+    '--ep': '--endpoint',
+  },
+}
+
+//
+// Main
+//
+
+try { main() } catch (err) {
+  error(err)
+}
+
+function main() {
+  var argv = [...pipy.argv]
+  var program = argv.shift()
+  var command = argv.shift()
+
+  if (!command || command.startsWith('-')) return errorInput(`missing command`)
+
+  var f = ({
+    help, config,
+    start, stop, run, invite, evict, join, leave,
+    get, describe, create, 'delete': deleteCmd,
+  })[command]
+
+  if (!f) return errorInput(`unknown command: '${command}'`)
+
+  return f(argv, program)
+}
+
+//
+// Errors
+//
+
+function error(err) {
+  println('ztm:', err.message || err)
+  pipy.exit(-1)
+}
+
+function errorInput(msg, command) {
+  throw `${msg}. Type 'ztm ${command ? 'help ' + command : 'help'}' for help.`
+}
+
+function errorObjectType(type, command) {
+  errorInput(`invalid object type '${type}' for command '${command}'`, command)
+}
+
+//
+// Command parsing utilities
+//
 
 function readWord(argv, meaning) {
   var word = argv.shift()
-  if (!word || word.startsWith('-')) throw InputError(`missing ${meaning}`)
+  if (!word || word.startsWith('-')) return errorInput(`missing ${meaning}`)
   return word
 }
 
@@ -89,7 +175,7 @@ function readObjectType(argv) {
     case 'port':
     case 'ports':
       return 'port'
-    default: throw InputError(`unknown object type '${type}'`)
+    default: return errorInput(`unknown object type '${type}'`)
   }
 }
 
@@ -107,7 +193,7 @@ function normalizeServiceName(name) {
       return name
     }
   }
-  throw InputError(`invalid service name '${name}'`)
+  return errorInput(`invalid service name '${name}'`)
 }
 
 function normalizePortName(name) {
@@ -127,97 +213,7 @@ function normalizePortName(name) {
       }
     }
   }
-  throw InputError(`invalid port name '${name}'`)
-}
-
-//
-// Options
-//
-
-var optionsConfig = {
-  defaults: {
-    '--ca': '',
-    '--agent': '',
-  },
-  shorthands: {},
-}
-
-var optionsCA = {
-  defaults: {
-    '--database': '~/ztm-ca.db',
-    '--listen': '0.0.0.0:9999',
-  },
-  shorthands: {
-    '-d': '--database',
-    '-l': '--listen',
-  },
-}
-
-var optionsHub = {
-  defaults: {
-    '--listen': '0.0.0.0:8888',
-    '--name': [],
-    '--ca': 'localhost:9999',
-  },
-  shorthands: {
-    '-l': '--listen',
-    '-n': '--name',
-  },
-}
-
-var optionsAgent = {
-  defaults: {
-    '--database': '~/ztm.db',
-    '--listen': '127.0.0.1:7777',
-  },
-  shorthands: {
-    '-d': '--database',
-    '-l': '--listen',
-  },
-}
-
-var optionsInvite = {
-  defaults: {
-    '--output': '',
-    '--bootstrap': [],
-  },
-  shorthands: {
-    '-o': '--output',
-    '-b': '--bootstrap',
-  },
-}
-
-var optionsJoin = {
-  defaults: {
-    '--as': '',
-    '--profile': '',
-  },
-  shorthands: {
-    '-p': '--profile',
-  },
-}
-
-var optionsService = {
-  defaults: {
-    '--host': '',
-    '--port': 0,
-  },
-  shorthands: {
-    '-h': '--host',
-    '-p': '--port',
-  },
-}
-
-var optionsPort = {
-  defaults: {
-    '--service': '',
-    '--endpoint': '',
-  },
-  shorthands: {
-    '-s': '--service',
-    '-e': '--endpoint',
-    '--ep': '--endpoint',
-  },
+  return errorInput(`invalid port name '${name}'`)
 }
 
 function parseOptions(meta, argv, command) {
@@ -237,37 +233,6 @@ function requiredOption(opts, name, command) {
 }
 
 //
-// Main
-//
-
-try { main() } catch (err) {
-  error(err)
-}
-
-function main() {
-  var argv = [...pipy.argv]
-  var program = argv.shift()
-  var command = argv.shift()
-
-  if (!command || command.startsWith('-')) throw InputError(`missing command`)
-
-  var f = ({
-    help, config,
-    start, stop, run, invite, evict, join, leave,
-    get, describe, create, 'delete': deleteCmd,
-  })[command]
-
-  if (!f) throw InputError(`unknown command: '${command}'`)
-
-  return f(argv, program)
-}
-
-function error(err) {
-  println('ztm:', err.message || err)
-  pipy.exit(-1)
-}
-
-//
 // Command: config
 //
 
@@ -277,7 +242,7 @@ function config(argv) {
     ca: opts['--ca'],
     agent: opts['--agent'],
   }
-  if (Object.values(conf).filter(i=>i).length === 0) throw InputError('missing options', 'config')
+  if (Object.values(conf).filter(i=>i).length === 0) return errorInput('missing options', 'config')
   clients.config(conf)
 }
 
@@ -291,7 +256,7 @@ function start(argv, program) {
     case 'ca': return startCA(argv, program)
     case 'hub': return startHub(argv, program)
     case 'agent': return startAgent(argv, program)
-    default: throw InvalidObjectType(type, 'start')
+    default: return errorObjectType(type, 'start')
   }
 }
 
@@ -320,7 +285,7 @@ function stop(argv) {
     case 'ca': return stopCA()
     case 'hub': return stopHub()
     case 'agent': return stopAgent()
-    default: throw InvalidObjectType(type, 'stop')
+    default: return errorObjectType(type, 'stop')
   }
 }
 
@@ -343,7 +308,7 @@ function run(argv, program) {
     case 'ca': return runCA(argv, program)
     case 'hub': return runHub(argv, program)
     case 'agent': return runAgent(argv, program)
-    default: throw InvalidObjectType(type, 'run')
+    default: return errorObjectType(type, 'run')
   }
 }
 
@@ -433,13 +398,13 @@ function join(argv) {
   var meshName = normalizeName(readWord(argv, 'mesh name'))
   var opts = parseOptions(optionsJoin, argv, 'join')
   var epName = normalizeName(requiredOption(opts, '--as', 'join'))
-  var profilePathname = requiredOption(opts, '--profile', 'join')
-  var profile = JSON.decode(os.read(profilePathname))
-  if (!profile.ca) throw 'profile missing CA certificate'
-  if (!profile.agent?.certificate) throw 'profile missing user certificate'
-  if (!profile.agent?.key) throw 'profile missing user key'
-  if (!(profile.bootstraps instanceof Array)) throw 'profile missing bootstraps'
-  if (profile.bootstraps.some(
+  var profilePathname = requiredOption(opts, '--permit', 'join')
+  var permit = JSON.decode(os.read(profilePathname))
+  if (!permit.ca) throw 'permit missing CA certificate'
+  if (!permit.agent?.certificate) throw 'permit missing user certificate'
+  if (!permit.agent?.key) throw 'permit missing user key'
+  if (!(permit.bootstraps instanceof Array)) throw 'permit missing bootstraps'
+  if (permit.bootstraps.some(
     addr => {
       if (typeof addr !== 'string') return true
       var i = addr.lastIndexOf(':')
@@ -451,13 +416,13 @@ function join(argv) {
   )) throw 'invalid bootstrap address'
   var c = clients.agent()
   c.post(`/api/meshes/${meshName}`, JSON.encode({
-    ca: profile.ca,
+    ca: permit.ca,
     agent: {
       name: epName,
-      certificate: profile.agent.certificate,
-      key: profile.agent.key,
+      certificate: permit.agent.certificate,
+      key: permit.agent.key,
     },
-    bootstraps: profile.bootstraps,
+    bootstraps: permit.bootstraps,
   })).then(() => {
     pipy.exit(0)
   }).catch(error)
@@ -486,7 +451,7 @@ function get(argv) {
     case 'endpoint': return getEndpoint(argv)
     case 'service': return getService(argv)
     case 'port': return getPort(argv)
-    default: throw InvalidObjectType(type, 'get')
+    default: return errorObjectType(type, 'get')
   }
 }
 
@@ -521,7 +486,7 @@ function describe(argv) {
     case 'endpoint': return describeEndpoint(argv)
     case 'service': return describeService(argv)
     case 'port': return describePort(argv)
-    default: throw InvalidObjectType(type, 'describe')
+    default: return errorObjectType(type, 'describe')
   }
 }
 
@@ -554,7 +519,7 @@ function create(argv) {
   switch (type) {
     case 'service': return createService(argv)
     case 'port': return createPort(argv)
-    default: throw InvalidObjectType(type, 'create')
+    default: return errorObjectType(type, 'create')
   }
 }
 
@@ -579,7 +544,7 @@ function deleteCmd(argv) {
   switch (type) {
     case 'service': return deleteService(argv)
     case 'port': return deletePort(argv)
-    default: throw InvalidObjectType(type, 'delete')
+    default: return errorObjectType(type, 'delete')
   }
 }
 
