f86dd7bf5abbcc88cf6a4ec295b9667616a3afbe
DongChen Lin <393227948@qq.com>
2024-05-28 14:08:56 +0800

    update android floatingView.visibility

--- gui/src-android/app/src/main/java/com/flomesh/ztm/FloatingWindowService.kt
+++ gui/src-android/app/src/main/java/com/flomesh/ztm/FloatingWindowService.kt
@@ -48,7 +48,7 @@ class FloatingWindowService : Service(), Application.ActivityLifecycleCallbacks 
 
     override fun onActivityResumed(activity: Activity) {
         Log.d("FloatingWindowService", "onActivityResumed")
-				floatingView.visibility = View.VISIBLE
+				floatingView.visibility = View.INVISIBLE
     }
 
     override fun onActivityPaused(activity: Activity) {
@@ -66,6 +66,7 @@ class FloatingWindowService : Service(), Application.ActivityLifecycleCallbacks 
 
     override fun onActivityDestroyed(activity: Activity) {
         Log.d("FloatingWindowService", "onActivityDestroyed")
+				floatingView.visibility = View.INVISIBLE
     }
 
     private fun requestOverlayPermissionIfNeeded() {
