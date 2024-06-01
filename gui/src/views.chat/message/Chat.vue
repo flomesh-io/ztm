7f6cc4ed089a6f78133b5640b3b501af478c9f57
DongChen Lin <393227948@qq.com>
2024-06-02 00:09:32 +0800

    fix chat media bug

--- gui/src/views.chat/message/Chat.vue
+++ gui/src/views.chat/message/Chat.vue
@@ -24,6 +24,8 @@ const viewHeight = computed(() => windowHeight.value - 36);
 const back = () => {
 	emits('back')
 }
+const hasMediaDevices = computed(() => !!navigator.mediaDevices && false);
+//navigator.mediaDevices
 </script>
 
 <template>
@@ -48,9 +50,15 @@ const back = () => {
 				"user": {"bubble": {"backgroundColor": "#9855f7"}}
 		}}'
 		:avatars="true"
+		:inputAreaStyle='{"backgroundColor": "#F4F6F7"}'
 		:textInput='{
 			placeholder: { text: "Welcome to the demo!" },
 			"styles": {
+				"container": {
+					"backgroundColor": "#fefefe",
+					"border": "none",
+					"boxShadow": "none"
+				},
 				"text": {"padding": "10px", "paddingLeft": "15px", "paddingRight": "34px"}
 			}
 		}'
@@ -59,8 +67,9 @@ const back = () => {
 			"submit": {
 				"container": {
 					"default": {
-						"width": "1.2em",
-						"height": "1.2em",
+						"top": "13px",
+						"width": "1.6em",
+						"height": "1.6em",
 						"justifyContent": "center",
 						"display": "flex",
 						"borderRadius": "25px",
@@ -69,8 +78,8 @@ const back = () => {
 						"backgroundColor": "unset",
 						
 					},
-					"hover": {"backgroundColor": "#b0deff4f","boxShadow": "0px 0.3px 0.9px rgba(0, 0, 0, 0.12), 0px 1.6px 3.6px rgba(0, 0, 0, 0.16)"},
-					"click": {"backgroundColor": "#b0deffb5","boxShadow": "0px 0.3px 0.9px rgba(0, 0, 0, 0.12), 0px 1.6px 3.6px rgba(0, 0, 0, 0.16)"}
+					"hover": {"backgroundColor": "#f4f3ff","boxShadow": "0px 0.3px 0.9px rgba(0, 0, 0, 0.12), 0px 1.6px 3.6px rgba(0, 0, 0, 0.16)"},
+					"click": {"backgroundColor": "#f4f3ff","boxShadow": "0px 0.3px 0.9px rgba(0, 0, 0, 0.12), 0px 1.6px 3.6px rgba(0, 0, 0, 0.16)"}
 				},
 				"svg": {
 					"content": "<?xml version=\"1.0\" encoding=\"utf-8\"?> <svg viewBox=\"0 0 24 24\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"m21.426 11.095-17-8A.999.999 0 0 0 3.03 4.242L4.969 12 3.03 19.758a.998.998 0 0 0 1.396 1.147l17-8a1 1 0 0 0 0-1.81zM5.481 18.197l.839-3.357L12 12 6.32 9.16l-.839-3.357L18.651 12l-13.17 6.197z\"/></svg>",
@@ -85,7 +94,7 @@ const back = () => {
 					
 			}
 		}'
-		:microphone='{
+		:microphone='hasMediaDevices?{
 			"button": {
 				"default": {
 					"container": {"default": {"bottom": "1em", "right": "0.6em", "borderRadius": "20px", "width": "1.9em", "height": "1.9em"}},
@@ -93,7 +102,7 @@ const back = () => {
 				},
 				"position": "inside-right"
 			}
-		}'
+		}:false'
 		auxiliaryStyle="
 			::-webkit-scrollbar-thumb {
 				background-color: #9855f7;
@@ -106,8 +115,8 @@ const back = () => {
 							"width": "2.2em",
 							"height": "2.2em",
 						},
-						"hover": {"backgroundColor": "#e4f3ff"},
-						"click": {"backgroundColor": "#d7edff"}
+						"hover": {"backgroundColor": "#f4f3ff"},
+						"click": {"backgroundColor": "#f7edff"}
 					},
 					"svg": {"styles": {"default": {"fontSize": "1.3em"}}}
 				}
@@ -118,10 +127,10 @@ const back = () => {
 				},
 				"item": {
 					"hover": {
-						"backgroundColor": "#e1f2ff"
+						"backgroundColor": "#f4f3ff"
 					},
 					"click": {
-						"backgroundColor": "#cfeaff"
+						"backgroundColor": "#f4f3ff"
 					}
 				},
 				"iconContainer": {
@@ -134,8 +143,24 @@ const back = () => {
 		}'
 		:demo="true"
 		:stream="true"
-		:images="true"
-		:camera='{"button": {"position": "dropup-menu"}}'
+		:images='{
+		
+			"button": {
+				"styles": {
+					"container": {
+						"default": {
+							"width": "2.2em",
+							"height": "2.2em",
+						},
+						"hover": {"backgroundColor": "#f4f3ff"},
+						"click": {"backgroundColor": "#f7edff"}
+					},
+					"svg": {"styles": {"default": {"fontSize": "1.3em"}}}
+				}
+			},
+			
+		}'
+		:camera='hasMediaDevices?{"button": {"position": "dropup-menu"}}:false'
 	  />
 	</div>
 </template>
