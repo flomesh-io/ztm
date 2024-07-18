5224af658b4361d701b0be2fa17a412e0d16cdbf
pajama-coder <pajamacoder@flomesh.io>
2024-07-18 17:59:35 +0800

    Support external CA service

--- ca/main.js
+++ ca/main.js
@@ -1,139 +1,97 @@
 #!/usr/bin/env -S pipy --args
 
-import db from './db.js'
-import options from './options.js'
+import cmdline from './cmdline.js'
 
-var opt = options(pipy.argv, {
-  defaults: {
-    '--help': false,
-    '--database': '~/ztm-ca.db',
-    '--listen': '127.0.0.1:9999',
-  },
-  shorthands: {
-    '-h': '--help',
-    '-d': '--database',
-    '-l': '--listen',
-  },
+cmdline(pipy.argv, {
+  commands: [{
+    title: 'A Sample of ZTM CA Service',
+    options: `
+      -l, --listen  <[ip:]port>   Service port to listen on (default: 127.0.0.1:9999)
+      -p, --path    <pathname>    Base path of the REST API (default: /api/certificates)
+    `,
+
+    action: (args) => {
+      var listen = args['--listen'] || '127.0.0.1:9999'
+      var basePath = args['--path'] || '/api/certificates'
+
+      var caKey = new crypto.PrivateKey({
+        type: 'rsa',
+        bits: 2048,
+      })
+
+      var caCert = new crypto.Certificate({
+        subject: { CN: 'ca' },
+        privateKey: caKey,
+        publicKey: new crypto.PublicKey(caKey),
+        days: 365,
+      })
+
+      var handler = service({
+
+        [`${basePath}/{name}`]: {
+          'GET': (params) => {
+            var name = params.name
+            if (name === 'ca') {
+              return response(200, caCert.toPEM().toString())
+            } else {
+              return response(404)
+            }
+          },
+
+          'POST': (params, req) => {
+            var name = params.name
+            var pkey = new crypto.PublicKey(req.body)
+            var cert = new crypto.Certificate({
+              subject: { CN: name },
+              issuer: caCert,
+              privateKey: caKey,
+              publicKey: pkey,
+              days: 365,
+            })
+            console.info(`Issued certificate to ${name}`)
+            return response(201, cert.toPEM().toString())
+          },
+        }
+
+      })
+
+      pipy.listen(listen, $=>$.serveHTTP(handler))
+    }
+  }]
 })
 
-if (opt['--help']) {
-  println('Options:')
-  println('  -h, --help      Show available options')
-  println('  -d, --database  Pathname of the database file (default: ~/ztm-ca.db)')
-  println('  -l, --listen    Port number to listen (default: 127.0.0.1:9999)')
-  return
-}
-
-var dbPath = opt['--database']
-if (dbPath.startsWith('~/')) {
-  dbPath = os.home() + dbPath.substring(1)
-}
-
-db.open(dbPath)
-
-//
-// Generate the CA certificate
-//
-
-if (!db.getKey('ca')) {
-  (function () {
-    var key = new crypto.PrivateKey({ type: 'rsa', bits: 2048 })
-    var cert = new crypto.Certificate({
-      subject: { CN: 'ca' },
-      privateKey: key,
-      publicKey: new crypto.PublicKey(key),
-      days: 365,
-    })
-    db.setCert('ca', cert.toPEM().toString())
-    db.setKey('ca', key.toPEM().toString())
-  })()
-}
-
-//
-// Retreive the current CA certificate
-//
-
-var caCert = new crypto.Certificate(db.getCert('ca'))
-var caKey = new crypto.PrivateKey(db.getKey('ca'))
-
-//
-// REST API paths & methods
-//
-
-var routes = Object.entries({
-
-  '/api/certificates/{name}': {
-    GET: (params) => {
-      var c = db.getCert(params.name)
-      if (!c) return responseError(404, 'Username not found')
-      return response(200, c)
-    },
-
-    POST: (params) => {
-      var name = params.name
-      if (isReservedName(name)) return responseError(403, 'Reserved username')
-      if (db.getCert(name)) return responseError(409, 'Username already exists')
-      var c = issueCertificate(name)
-      db.setCert(name, c.cert.toPEM().toString());
-      return response(200, c.key.toPEM().toString())
-    },
-
-    DELETE: (params) => {
-      var name = params.name
-      if (isReservedName(name)) return responseError(403, 'Reserved username')
-      db.delCert(name)
-      return response(204)
-    },
-  },
-
-  '/api/sign/hub/{name}': {
-    POST: (params, req) => {
-      var pkey = new crypto.PublicKey(req.body)
-      var cert = signCertificate(params.name, pkey)
-      return response(200, cert.toPEM().toString())
-    }
-  },
-
-}).map(
-  function ([path, methods]) {
-    var match = new http.Match(path)
-    var handler = function (params, req) {
-      var f = methods[req.head.method]
-      if (f) return f(params, req)
-      return new Message({ status: 405 })
-    }
-    return { match, handler }
-  }
-)
-
-//
-// Handle requests
-//
-
-var $isFromLocal
-
-pipy.listen(opt['--listen'], $=>$
-  .onStart(ib => { $isFromLocal = (ib.localAddress === '127.0.0.1') })
-  .serveHTTP(
-    function (req) {
-      var path = req.head.path
-      var params = null
-      var route = routes.find(r => Boolean(params = r.match(path)))
-      if (route) {
-        try {
-          return route.handler(params, req)
-        } catch (e) {
-          return response(500, e)
-        }
+function service(routes) {
+  routes = Object.entries(routes).map(
+    function ([path, methods]) {
+      var match = new http.Match(path)
+      var handler = function (params, req) {
+        var f = methods[req.head.method]
+        if (f) return f(params, req)
+        return response(405)
       }
-      return new Message({ status: 404 })
+      return { match, handler }
     }
   )
-)
+
+  return (req) => {
+    var path = req.head.path
+    var params = null
+    var route = routes.find(r => Boolean(params = r.match(path)))
+    if (route) {
+      try {
+        return route.handler(params, req)
+      } catch (e) {
+        return response(500, e)
+      }
+    }
+    return response(404)
+  }
+}
 
 function response(status, body) {
   if (!body) return new Message({ status })
   if (typeof body === 'string') return responseCT(status, 'text/plain', body)
+  if (body instanceof Data) return responseCT(status, 'application/octet-stream', body)
   return responseCT(status, 'application/json', JSON.encode(body))
 }
 
@@ -145,33 +103,4 @@ function responseCT(status, ct, body) {
     },
     body
   )
-}
-
-function responseError(status, message) {
-  return response(status, { status, message })
-}
-
-function isReservedName(name) {
-  return (name === 'ca' || isHubName(name))
-}
-
-function isHubName(name) {
-  return (name === 'hub' || name.startsWith('hub/'))
-}
-
-function issueCertificate(name) {
-  var key = new crypto.PrivateKey({ type: 'rsa', bits: 2048 })
-  var pkey = new crypto.PublicKey(key)
-  var cert = signCertificate(name, pkey)
-  return { cert, key }
-}
-
-function signCertificate(name, pkey) {
-  return new crypto.Certificate({
-    subject: { CN: name },
-    days: 365,
-    issuer: caCert,
-    privateKey: caKey,
-    publicKey: pkey,
-  })
 }
--- cli/ca.js
+++ cli/ca.js
@@ -2,53 +2,77 @@ import db from './db.js'
 
 var caCert
 var caKey
+var caURL
 
-function init() {
-  if (!db.getKey('ca')) {
-    var key = new crypto.PrivateKey({ type: 'rsa', bits: 2048 })
-    var cert = new crypto.Certificate({
-      subject: { CN: 'ca' },
-      privateKey: key,
-      publicKey: new crypto.PublicKey(key),
-      days: 365,
-    })
-    db.setCert('ca', cert.toPEM().toString())
-    db.setKey('ca', key.toPEM().toString())
+function init(url) {
+  if (url) {
+    caURL = new URL(url)
+  } else {
+    if (!db.getKey('ca')) {
+      var key = new crypto.PrivateKey({ type: 'rsa', bits: 2048 })
+      var cert = new crypto.Certificate({
+        subject: { CN: 'ca' },
+        privateKey: key,
+        publicKey: new crypto.PublicKey(key),
+        days: 365,
+      })
+      db.setCert('ca', cert.toPEM().toString())
+      db.setKey('ca', key.toPEM().toString())
+    }
+
+    caCert = new crypto.Certificate(db.getCert('ca'))
+    caKey = new crypto.PrivateKey(db.getKey('ca'))
   }
-
-  caCert = new crypto.Certificate(db.getCert('ca'))
-  caKey = new crypto.PrivateKey(db.getKey('ca'))
-
   return Promise.resolve()
 }
 
 function getCertificate(name) {
-  var pem = db.getCert(name)
-  var crt = pem ? new crypto.Certificate(pem) : null
-  return Promise.resolve(crt)
-}
-
-function issueCertificate(name) {
-  var key = new crypto.PrivateKey({ type: 'rsa', bits: 2048 })
-  var pkey = new crypto.PublicKey(key)
-  var cert = signCertificate(name, pkey)
-  return Promise.resolve({ cert, key })
+  if (caURL) {
+    var url = caURL
+    var ha = new http.Agent(url.host)
+    return ha.request(
+      'GET', os.path.join(url.path, name)
+    ).then(res => {
+      var status = res?.head?.status
+      if (200 <= status && status <= 299) {
+        return new crypto.Certificate(res.body.toString())
+      }
+      return null
+    })
+  } else {
+    var pem = db.getCert(name)
+    var crt = pem ? new crypto.Certificate(pem) : null
+    return Promise.resolve(crt)
+  }
 }
 
 function signCertificate(name, pkey) {
-  var crt = new crypto.Certificate({
-    subject: { CN: name },
-    days: 365,
-    issuer: caCert,
-    privateKey: caKey,
-    publicKey: pkey,
-  })
-  return Promise.resolve(crt)
+  if (caURL) {
+    var url = caURL
+    var ha = new http.Agent(url.host)
+    return ha.request(
+      'POST', os.path.join(url.path, name), null, pkey.toPEM()
+    ).then(res => {
+      var status = res?.head?.status
+      if (200 <= status && status <= 299) {
+        return new crypto.Certificate(res.body.toString())
+      }
+      return null
+    })
+  } else {
+    var crt = new crypto.Certificate({
+      subject: { CN: name },
+      days: 365,
+      issuer: caCert,
+      privateKey: caKey,
+      publicKey: pkey,
+    })
+    return Promise.resolve(crt)
+  }
 }
 
 export default {
   init,
   getCertificate,
-  issueCertificate,
   signCertificate,
 }
