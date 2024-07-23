d4ef836260ff1914713e62f8e3e81112fafdb98e
pajama-coder <pajamacoder@flomesh.io>
2024-07-23 19:48:28 +0800

    Added GUI build commands for builtin apps

--- build/gui.cmd
+++ build/gui.cmd
@@ -8,6 +8,9 @@ SET ztm_dir=%CD%
 
 CD "%ztm_dir%\gui"
 CMD /c "npm run build"
+CMD /c "npm run build:tunnel"
+CMD /c "npm run build:proxy"
+CMD /c "npm run build:script"
 
 CD "%ztm_dir%\pipy"
 IF EXIST build\deps\codebases.tar.gz.h (DEL build\deps\codebases.tar.gz.h)
--- build/gui.sh
+++ build/gui.sh
@@ -4,5 +4,8 @@ ZTM_DIR=$(cd "$(dirname "$0")" && cd .. && pwd)
 
 cd "$ZTM_DIR/gui"
 npm run build
+npm run build:tunnel
+npm run build:proxy
+npm run build:script
 
 rm -f "$ZTM_DIR/pipy/build/deps/codebases.tar.gz.h"
