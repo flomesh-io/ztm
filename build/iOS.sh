5c183351fc8ccadf87f9a5bc3f6cd695027cd58e
York Wong <97939737+nixff@users.noreply.github.com>
2024-09-06 14:15:20 +0800

    [build] add libpipy.a script for iOS

---
+++ build/iOS.sh
@@ -0,0 +1,67 @@
+BUILD_SCRIPT_DIR=$(dirname "$(realpath $0)")
+ZTM_DIR=$(dirname $BUILD_SCRIPT_DIR)
+cd $ZTM_DIR
+
+# check if xcode installed
+xcodebuild -version > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+  echo "Please install xcode."
+  eixt 1
+fi
+
+export IOS_SDK_VERSION=$(xcrun -sdk iphoneos --show-sdk-version)
+
+# clone and build openssl-apple
+pushd pipy
+if [ -d openssl-apple ]; then
+  pushd openssl-apple
+  git pull
+  git checkout v11
+  popd
+else
+  git clone --branch v11 https://github.com/passepartoutvpn/openssl-apple.git
+fi
+pushd openssl-apple
+./build-libssl.sh --version=3.2.0 --targets="ios64-cross-arm64" --deprecated
+if [ ! -f bin/iPhoneOS17.5-arm64.sdk/lib/libssl.a ]; then
+  echo "Build libssl for iOS failed."
+  exit 1
+fi
+
+LIBSSL_PATH=$(realpath $PWD/bin/iPhoneOS${IOS_SDK_VERSION}-arm64.sdk)
+popd
+
+# config pipy
+rm -rf build && mkdir build
+pushd build
+cmake .. -DCMAKE_SYSTEM_NAME=iOS \
+  -DPIPY_OPENSSL=${LIBSSL_PATH} \
+  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+  -DPIPY_STATICLIB=ON \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DPIPY_GUI=OFF \
+  -DPIPY_CODEBASES=ON \
+  -DPIPY_CUSTOM_CODEBASES=ztm/ca:../ca,ztm/hub:../hub,ztm/agent:../agent,ztm/cli:../cli \
+  -DCMAKE_OSX_DEPLOYMENT_TARGET=12.0 \
+  -GXcode
+
+if [ $? -ne 0 ]; then
+  echo "Configure failed for ztm."
+  exit 1
+fi
+
+# build libpipy.a
+cmake --build .  --config Release -t pipy
+
+if [ $? -ne 0 ]; then
+  echo 'Compile libpipy.a failed.'
+  exit 1
+fi
+
+if [ ! -d $ZTM_DIR/usr/local/lib ]; then
+  mkdir -p $ZTM_DIR/usr/local/lib
+fi
+
+cp Release-iphoneos/libpipy.a $ZTM_DIR/usr/local/lib/ && \
+  echo "The libpipy.a is ready at $ZTM_DIR/usr/local/lib/libpipy.a"
+popd
