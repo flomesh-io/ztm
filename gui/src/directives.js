a412bb16b4db2753dc052b58b4db5707fe5590b5
DongChen Lin <393227948@qq.com>
2024-09-07 12:22:56 +0800

    update cloud menu

--- gui/src/directives.js
+++ gui/src/directives.js
@@ -2,10 +2,13 @@ import StyleClass from 'primevue/styleclass';
 import Ripple from 'primevue/ripple';
 import Tooltip from 'primevue/tooltip';
 import BadgeDirective from 'primevue/badgedirective';
+import longtap from './directives/longtap';
+
 
 export function useDirective(app){
 	app.directive('tooltip', Tooltip);
 	app.directive('badge', BadgeDirective);
 	app.directive('ripple', Ripple);
 	app.directive('styleclass', StyleClass);
+	app.directive('longtap', longtap);
 }
