8f13a947d8b6295f83e825d7e83e4750ad20f510
DongChen Lin <393227948@qq.com>
2024-09-18 17:55:08 +0800

    update gui

--- gui/src/views/pages/NotFound.vue
+++ gui/src/views/pages/NotFound.vue
@@ -4,12 +4,12 @@
 <template>
     <div class="surface-ground flex align-items-center justify-content-center min-h-screen min-w-screen overflow-hidden">
         <div class="flex flex-column align-items-center justify-content-center">
-            <div style="border-radius: 56px; padding: 0.3rem; background: linear-gradient(180deg, rgba(33, 150, 243, 0.4) 10%, rgba(33, 150, 243, 0) 30%)">
+            <div style="border-radius: 56px; padding: 0.3rem; background: linear-gradient(180deg, rgba(255, 255, 255, 0.4) 10%, rgba(255, 255, 255, 0.4) 30%)">
                 <div class="w-full surface-card py-8 px-5 sm:px-8 flex flex-column align-items-center" style="border-radius: 53px">
                     <span class="text-blue-500 font-bold text-3xl">404</span>
                     <h1 class="text-900 font-bold text-3xl lg:text-5xl mb-2">Not Found</h1>
                     <div class="text-600 mb-5">Requested resource is not available.</div>
-                    <router-link to="/" class="w-full flex align-items-center py-5 border-300 border-bottom-1">
+                    <!-- <router-link to="/" class="w-full flex align-items-center py-5 border-300 border-bottom-1">
                         <span class="flex justify-content-center align-items-center bg-cyan-400 border-round" style="height: 3.5rem; width: 3.5rem">
                             <i class="text-50 pi pi-fw pi-table text-2xl"></i>
                         </span>
@@ -35,7 +35,7 @@
                             <span class="text-900 lg:text-xl font-medium mb-0">Permission Manager</span>
                             <span class="text-600 lg:text-xl">Accumsan in nisl nisi scelerisque</span>
                         </span>
-                    </router-link>
+                    </router-link> -->
                 </div>
             </div>
         </div>
