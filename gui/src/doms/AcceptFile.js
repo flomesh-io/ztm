d18997913f341b9ee2f0b2e1a98654a6fcc0b230
DongChen Lin <393227948@qq.com>
2024-11-06 20:18:15 +0800

    fix chat file saving progress

--- gui/src/doms/AcceptFile.js
+++ gui/src/doms/AcceptFile.js
@@ -16,36 +16,79 @@ class AcceptFile extends HTMLElement {
 				.state{
 					padding:6px;
 					cursor: pointer;
+					position:relative;
 				}
-				
+				.done{
+					position: absolute;
+					top:10px;
+					left:10px;
+					opacity:0.9;
+					z-index:3;
+				}
 				.loading,.done,.uploading{
 					display:none;
 				}
 				.progress{
 					position: absolute;
-				  width: 36px;
-				  height: 36px;
+				  width: 32px;
+				  height: 32px;
 					text-align: center;
 					font-size:8pt;
 					opacity:0.8;
-					line-height:36px;
+					line-height:32px;
 				}
-				.spinner{
-					--size: 36px; 
-					--progress-color: rgba(0, 0, 0, 0.4); 
-					--background-color: rgba(0, 0, 0, 0.2);
-					--progress: 0; /* 进度百分比 */
-				
+				.spinner {
+					--size: 32px; 
+					--progress-color: #4E315F; 
+					--background-color: #82529D;
+					--progress: 0; 
+					
 					width: var(--size);
 					height: var(--size);
 					border-radius: 50%;
-					background: conic-gradient(
-						var(--progress-color) calc(var(--progress) * 1%),
-						var(--background-color) 0
-					);
-					transition: background 0.5s ease-out;
+					background-color: var(--background-color);
+					position: relative;
+					overflow: hidden;
 				}
 				
+				.spinner-left {
+				    position: absolute;
+						z-index:2;
+				    top: 0;
+				    left: 0;
+				    width: 100%;
+				    height: 100%;
+				    background-color: var(--progress-color);
+				    clip-path: inset(0 50% 0 0); 
+						transform: rotate(calc(180deg));
+				    // transform-origin: center right; 
+				    transition: transform 0.1s ease; 
+				}
+				
+				.spinner-right {
+						z-index:1;
+				    position: absolute;
+				    top: 0;
+				    right: 0;
+				    width: 100%;
+				    height: 100%;
+				    background-color: var(--progress-color);
+				    clip-path: inset(0 50% 0 0); 
+				    // transform-origin: center left;
+				    transition: all 0.1s ease;
+						opacity:1;
+						transform: rotate(calc((var(--progress)) * 3.6deg));
+				}
+				
+				.spinner[data-progress='less-than-50'] .spinner-left {
+						z-index:1;
+						transform: rotate(calc((var(--progress)) * 3.6deg));
+				}
+				.spinner[data-progress='less-than-50'] .spinner-right {
+						z-index:2;
+				    background-color: var(--background-color);
+				    transform: rotate(calc(0deg));
+				}
 				.uploader{
 				  width: 28px;
 				  height: 28px;
@@ -61,7 +104,7 @@ class AcceptFile extends HTMLElement {
 				  }
 				}
 			</style>
-			<div class="download-button " style="display:flex">
+			<div class="download-button" style="display:flex;padding-top:3px">
 				<slot name="icon"></slot>
 				<div style="flex:1">
 					<div class="file-inner">
@@ -73,13 +116,16 @@ class AcceptFile extends HTMLElement {
 					<svg t="1730720588146" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="29082" width="30" height="30"><path d="M490.666667 490.666667m-490.666667 0a490.666667 490.666667 0 1 0 981.333333 0 490.666667 490.666667 0 1 0-981.333333 0Z" fill="#82529d" p-id="29083" data-spm-anchor-id="a313x.search_index.0.i25.a9e13a81DL4NdQ" class=""></path><path d="M647.381333 473.002667a26.88 26.88 0 0 0-24.917333-17.877334h-53.632c-0.298667 0-0.341333-0.512-0.341333-0.810666V327.125333a28.714667 28.714667 0 0 0-27.093334-29.610666h-78.592a28.501333 28.501333 0 0 0-26.922666 29.610666v127.146667c0 0.298667-0.298667 0.853333-0.554667 0.853333H382.506667a26.794667 26.794667 0 0 0-24.917334 17.834667 30.762667 30.762667 0 0 0 5.845334 31.744l120.021333 129.877333a25.6 25.6 0 0 0 38.4 0l120.021333-129.834666a30.72 30.72 0 0 0 5.504-31.744z" fill="#ffffff" p-id="29084"></path><path d="M661.077333 667.690667H343.808a13.269333 13.269333 0 1 0 0 26.496h317.269333a13.269333 13.269333 0 1 0 0-26.496z" fill="#ffffff" p-id="29085"></path></svg>
 				</div>
 				<div class="state loading">
-					<div class="spinner"></div>
+					<div class="spinner">
+							<div class="spinner-half spinner-left"></div>
+							<div class="spinner-half spinner-right"></div>
+					</div>
+					<div class="done">
+						<svg t="1730889960198" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="51051" width="22" height="22"><path d="M0 0h1024v1024H0z" fill="#D8D8D8" fill-opacity="0" p-id="51052"></path><path d="M895.561143 181.833143a52.809143 52.809143 0 0 1 78.116571 71.094857l-539.940571 593.188571a52.809143 52.809143 0 0 1-76.946286 1.316572L51.492571 533.577143A52.809143 52.809143 0 1 1 127.268571 459.922286l266.093715 273.554285 502.198857-551.643428z" fill="#44BB55" p-id="51053"></path></svg>
+					</div>
 				</div>
 				<div class="state uploading">
 					<div class="uploader"></div>
-				</div>
-				<div class="state done">
-					<svg t="1730621033625" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="18207" width="26" height="26"><path d="M512 512m-512 0a512 512 0 1 0 1024 0 512 512 0 1 0-1024 0Z" fill="#D5F7E0" p-id="18208"></path><path d="M407.552 586.752L239.616 455.68 163.84 515.072l292.864 294.912c51.2-126.976 208.896-376.832 403.456-555.008l-18.432-40.96c-210.944 126.976-364.544 290.816-434.176 372.736z" fill="#0AAD06" p-id="18209"></path></svg>
 				</div>
 			</div>
 		`;
@@ -103,14 +149,25 @@ class AcceptFile extends HTMLElement {
 				fileUrl:this.getAttribute('src'),
 				before:()=>{
 					this.setAttribute('progress',5);
-					this.updateProgress();
+					this.updateElement('loading');
 				},
 				after:(resp)=>{
-					if(resp){
-						this.doneProgress();
-					} else {
-						this.resetProgress()
-					}
+					setTimeout(()=>{
+						if(resp){
+							this.doneProgress();
+						} else {
+							this.resetProgress()
+						}
+					},600)
+				},
+				progressHandler:(resp)=>{
+					const size = this.getAttribute('size') * 1;
+					const percent = Math.floor((resp?.progress * 100)/size);
+					this.setAttribute('progress',percent<5?5:percent);
+					this.updateElement('loading');
+				},
+				headers: {
+					"Content-type": this.getAttribute('contentType')
 				}
 			})
 		} else {
@@ -128,21 +185,8 @@ class AcceptFile extends HTMLElement {
 			this.save(e)
 		});
 		this.shadowRoot.querySelector('.done').addEventListener('click', (e) => {
-			this.save(e)
+			this.resetProgress();
 		});
-	}
-	updateProgress() {
-		this.updateElement('loading');
-		setTimeout(()=>{
-			const progress = (this.getAttribute('progress')||5)*1;
-			const speed = downloadSpeed(this.getAttribute('size')*1);
-			if(progress>0 && progress < 99){
-				const next = (progress + speed) >= 100 ? 99 :(progress + speed);
-				this.setAttribute('progress', next);
-				this.updateProgress();
-			}
-		},1000)
-		
 	}
 	doneProgress() {
 		this.setAttribute('progress',100);
@@ -164,20 +208,26 @@ class AcceptFile extends HTMLElement {
 		const uploading = this.shadowRoot.querySelector('.uploading');
 		if(state == 'loading'){
 			download.style.display = "none";
+			const spinner = this.shadowRoot.querySelector('.spinner');
 			const progress = this.getAttribute('progress') * 1
 			if(progress>=0 && progress<100){
 				const loading = this.shadowRoot.querySelector('.loading');
 				loading.style.display = "block";
-				const spinner = this.shadowRoot.querySelector('.spinner');
+				const done = this.shadowRoot.querySelector('.done');
+				done.style.display = "none";
 				spinner.style.setProperty("--progress",progress<5?5:Math.floor(progress));
 			} else if (progress>=100){
 				const loading = this.shadowRoot.querySelector('.loading');
 				loading.style.setProperty("--progress", 100);
-				loading.style.display = "none";
+				loading.style.display = "block";
 				const done = this.shadowRoot.querySelector('.done');
 				done.style.display = "block";
-				const spinner = this.shadowRoot.querySelector('.spinner');
 				spinner.style.setProperty("--progress", 100);
+			}
+			if (progress <= 50) {
+				spinner.setAttribute('data-progress', 'less-than-50');
+			} else {
+				spinner.removeAttribute('data-progress');
 			}
 		} else {
 			if(!!this.getAttribute('src')){
