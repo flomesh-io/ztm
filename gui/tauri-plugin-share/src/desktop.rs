0bec0470201b8b52c9a23c1268350108d88e77b3
zhangcheng870518 <zhangcheng@flomesh.cn>
2024-11-08 17:48:49 +0800

    add tauri plugins

---
+++ gui/tauri-plugin-share/src/desktop.rs
@@ -0,0 +1,22 @@
+use serde::de::DeserializeOwned;
+use tauri::{plugin::PluginApi, AppHandle, Runtime};
+
+use crate::models::*;
+
+pub fn init<R: Runtime, C: DeserializeOwned>(
+  app: &AppHandle<R>,
+  _api: PluginApi<R, C>,
+) -> crate::Result<Share<R>> {
+  Ok(Share(app.clone()))
+}
+
+/// Access to the share APIs.
+pub struct Share<R: Runtime>(AppHandle<R>);
+
+impl<R: Runtime> Share<R> {
+  pub fn ping(&self, payload: PingRequest) -> crate::Result<PingResponse> {
+    Ok(PingResponse {
+      value: payload.value,
+    })
+  }
+}
