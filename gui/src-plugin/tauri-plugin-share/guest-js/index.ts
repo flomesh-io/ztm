df07072578796fcccf18ad1bcb5e239d7cc374ad
DongChen Lin <393227948@qq.com>
2024-11-09 13:35:33 +0800

    ignore plugin .tauri

--- gui/src-plugin/tauri-plugin-share/guest-js/index.ts
+++ gui/src-plugin/tauri-plugin-share/guest-js/index.ts
@@ -2,9 +2,7 @@ import { invoke } from '@tauri-apps/api/core'
 
 export async function share(path: string, mimeType: string): Promise<string | null> {
   return await invoke<{value?: string}>('plugin:share|share', {
-    payload: {
-      path,
-      mimeType
-    },
+		path,
+		mimeType
   }).then((r) => (r.value ? r.value : null));
 }
