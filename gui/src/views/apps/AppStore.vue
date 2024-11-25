27eda64709f39220e4316b009cd7b82d7290f030
DongChen Lin <393227948@qq.com>
2024-11-25 23:26:23 +0800

    update

--- gui/src/views/apps/AppStore.vue
+++ gui/src/views/apps/AppStore.vue
@@ -1,8 +1,9 @@
 <script setup>
 import { onMounted, reactive, ref, watch } from 'vue';
-import { useRouter } from 'vue-router';
+import { useRouter, useRoute } from 'vue-router';
 import { useStore } from 'vuex';
 import Apps from '@/views/apps/Apps.vue';
+const route = useRoute();
 const store = useStore();
 const router = useRouter();
 
@@ -10,6 +11,14 @@ const go = (path) => {
 	router.push(path);
 }
 const isTauri = ref(!!window.__TAURI_INTERNALS__ );
+
+onMounted(()=>{
+	window.addEventListener('popstate', () => {
+		setTimeout(()=>{
+			store.commit('notice/setApp', null);
+		},100)
+	});
+})
 </script>
 
 <template>
