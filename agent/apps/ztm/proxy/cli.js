78fb18cf760f8880a4fe1a59dbd727fbd850d906
pajama-coder <pajamacoder@flomesh.io>
2025-03-18 18:27:53 +0800

    Updated CLI for the proxy app

--- agent/apps/ztm/proxy/cli.js
+++ agent/apps/ztm/proxy/cli.js
@@ -1,4 +1,4 @@
-export default function ({ api, utils }) {
+export default function ({ api, mesh, utils }) {
   return pipeline($=>$
     .onStart(ctx => main(ctx))
   )
@@ -35,10 +35,7 @@ export default function ({ api, utils }) {
               --add-exclusion     <domain|ip ...>   Add excluded targets where traffic can't go via the endpoint
                                                     e.g. '*.example.com' and '8.88.0.0/16'
               --remove-exclusion  <domain|ip ...>   Remove previously added exclusions
-              --add-allow         <domain|ip ...>   Add allowed destinations
-              --remove-allow      <domain|ip ...>   Remove allowed destinations
-              --add-deny          <domain|ip ...>   Add denied destinations
-              --remove-deny       <domain|ip ...>   Remove denied destinations
+
               --gen-cert          <on|off>          Enable/disable certificate generation
               --set-log           <splunk|off>      Enable/disable logging
               --log-address       <host:port>       Set the address of the logging service, used with --set-log
@@ -82,40 +79,6 @@ export default function ({ api, utils }) {
               if ('--remove-exclusion' in args && config.exclusions instanceof Array) {
                 args['--remove-exclusion'].forEach(target => {
                   config.exclusions = config.exclusions.filter(t => t !== target)
-                })
-                changed = true
-              }
-
-              if ('--add-allow' in args) {
-                if (!(config.allow instanceof Array)) config.allow = []
-                args['--add-allow'].forEach(allow => {
-                  if (!config.allow.includes(allow)) {
-                    config.allow.push(allow)
-                  }
-                })
-                changed = true
-              }
-
-              if ('--remove-allow' in args && config.allow instanceof Array) {
-                args['--remove-allow'].forEach(allow => {
-                  config.allow = config.allow.filter(t => t !== allow)
-                })
-                changed = true
-              }
-
-              if ('--add-deny' in args) {
-                if (!(config.deny instanceof Array)) config.deny = []
-                args['--add-deny'].forEach(deny => {
-                  if (!config.deny.includes(deny)) {
-                    config.deny.push(deny)
-                  }
-                })
-                changed = true
-              }
-
-              if ('--remove-deny' in args && config.deny instanceof Array) {
-                args['--remove-deny'].forEach(deny => {
-                  config.deny = config.deny.filter(t => t !== deny)
                 })
                 changed = true
               }
@@ -165,18 +128,6 @@ export default function ({ api, utils }) {
                     output('Excluded Targets:\n')
                     config.exclusions.forEach(t => output(`  ${t}\n`))
                   }
-                  if (config.allow instanceof Array && config.allow.length > 0) {
-                    output('Allow:\n')
-                    config.allow.forEach(t => output(`  ${t}\n`))
-                  } else {
-                    output('Allow: All\n')
-                  }
-                  if (config.deny instanceof Array && config.deny.length > 0) {
-                    output('Deny:\n')
-                    config.deny.forEach(t => output(`  ${t}\n`))
-                  } else {
-                    output('Deny: None\n')
-                  }
                 } else {
                   output('Targets: (not an exit)\n')
                 }
@@ -189,6 +140,116 @@ export default function ({ api, utils }) {
                 } else {
                   output(`Logging: Off\n`)
                 }
+              }
+            })
+          },
+
+          {
+            title: 'View or change proxy rules',
+            usage: 'rules',
+            options: `
+              --index     <number>          Open/close the proxy port
+              --insert                      Insert a new rule at the given index
+              --delete                      Delete the rule at the given index
+              --users     <username ...>    Match specified users
+              --groups    <group ...>       Match specified user groups
+              --targets   <domain|ip ...>   Match specified targets
+                                            e.g. '*.example.com' and '8.88.0.0/16'
+              --action    <allow|deny>      Specify the action to take
+            `,
+            action: (args) => api.getEndpointConfig(endpoint.id).then(config => {
+              if (!(config.rules instanceof Array)) config.rules = []
+
+              var index = args['--index']
+              var insert = args['--insert']
+              var remove = args['--delete']
+              var users = args['--users']
+              var groups = args['--groups']
+              var targets = args['--targets']
+              var action = args['--action']
+
+              if (!index && !insert && !remove && !users && !groups && !targets && !action) {
+                return printRules(config.rules || [])
+              }
+
+              if (!index) throw `Missing option --index`
+
+              index = Number.parseInt(index)
+              if (Number.isNaN(index)) throw `Invalid value for option --index`
+              if (index < 0 || index > config.rules.length) throw `Value out of range for option --index`
+
+              if (remove) {
+                if (insert || users || groups || targets || action) throw `Option --delete can only be used alone`
+                config.rules.splice(index, 1)
+                return api.setEndpointConfig(endpoint.id, config).then(
+                  () => printRules(config.rules)
+                )
+              }
+
+              if (!action) throw `Missing option --action`
+              if (action !== 'allow' && action !== 'deny') throw `Invalid value for option --action`
+
+              var allGroups = []
+
+              return pipeline($=>$
+                .onStart(new Message({ method: 'GET', path: '/api/groups' }))
+                .muxHTTP().to($=>$.pipe(mesh.connect({ app: 'users' })))
+                .replaceMessage(res => {
+                  if (res?.head?.status === 200) {
+                    allGroups = JSON.decode(res.body)
+                  }
+                  return new StreamEnd
+                })
+              ).spawn().then(() => {
+                if (groups) {
+                  groups = groups.map(name => {
+                    var matches = allGroups.filter(g => g.name === name)
+                    if (matches.length === 0) throw `No user group '${name}'`
+                    if (matches.length > 1) throw `Ambiguous user group '${name}'`
+                    return matches[0].id
+                  })
+                }
+                var rule = { targets, users, groups, action }
+                if (insert) {
+                  config.rules.splice(index, 0, rule)
+                } else {
+                  config.rules[index] = rule
+                }
+                return api.setEndpointConfig(endpoint.id, config).then(
+                  () => printRules(config.rules)
+                )
+              })
+
+              function printRules(rules) {
+                var groups = {}
+                rules.forEach(rule => {
+                  if (rule.groups instanceof Array) {
+                    rule.groups.forEach(gid => groups[gid] = null)
+                  }
+                })
+                var $result = null
+                var queryGroupName = pipeline($=>$
+                  .onStart(gid => new Message({ method: 'GET', path: `/api/groups/${gid}` }))
+                  .muxHTTP().to($=>$.pipe(mesh.connect({ app: 'users' })))
+                  .handleMessage(res => $result = (res?.head?.status === 200 ? JSON.decode(res.body).name : null))
+                  .replaceMessage(new StreamEnd)
+                  .onEnd(() => $result)
+                )
+                return Promise.all(
+                  Object.keys(groups).map(
+                    gid => queryGroupName.spawn(gid).then(
+                      name => groups[gid] = name
+                    )
+                  )
+                ).then(() => {
+                  printTable(rules, {
+                    '': (_, i) => '#' + i,
+                    'TARGETS': r => r.targets?.length > 0 ? r.targets.join(' ') : '-',
+                    'USERS': r => r.users?.length > 0 ? r.users.join(' ') : '-',
+                    'GROUPS': r => r.groups?.length > 0 ? r.groups.map(gid => groups[gid] || gid).join(' ') : '-',
+                    'ACTION': r => r.action,
+                  })
+                })
               }
             })
           },
@@ -212,6 +273,26 @@ export default function ({ api, utils }) {
     } catch (err) {
       error(err)
       return flush()
+    }
+
+    function printTable(data, columns) {
+      var cols = Object.entries(columns)
+      var colHeaders = cols.map(i => i[0])
+      var colFormats = cols.map(i => i[1])
+      var colSizes = colHeaders.map(name => name.length)
+      var rows = data.map((row, i) => colFormats.map(
+        (format, j) => {
+          var v = (format(row, i) || '').toString()
+          colSizes[j] = Math.max(colSizes[j], v.length)
+          return v
+        }
+      ))
+      colHeaders.forEach((name, i) => output(name.padEnd(colSizes[i] + 2)))
+      output('\n')
+      rows.forEach(row => {
+        row.forEach((v, i) => output(v.padEnd(colSizes[i] + 2)))
+        output('\n')
+      })
     }
   }
 }
