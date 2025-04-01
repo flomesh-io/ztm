9f8b8b5ed62c102854d8239e131df60be396a61c
pajama-coder <pajamacoder@flomesh.io>
2025-04-01 12:40:07 +0800

    Added interfaces for querying agent and hub versions

--- build/deps.cmd
+++ build/deps.cmd
@@ -26,5 +26,6 @@ FOR /f %%i IN ('git describe --abbrev^=0 --tags') DO SET VERSION=%%i
 FOR /f %%i IN ('git log -1 --format^=%%H') DO SET COMMIT=%%i
 FOR /f "eol= tokens=* delims=" %%i IN ('"git log -1 --format=%%cD"') DO SET COMMIT_DATE=%%i
 
-ECHO {"version":"%VERSION%","commit":"%COMMIT%","date":"%OMMIT_DATE%"} > cli\version.json
+ECHO {"tag":"%VERSION%","commit":"%COMMIT%","date":"%OMMIT_DATE%"} > cli\version.json
+COPY cli\version.json hub\version.json
 COPY cli\version.json agent\version.json
