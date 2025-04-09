74582b489204940d05f24591738f51bb85fb932f
pajama-coder <pajamacoder@flomesh.io>
2025-04-09 12:02:51 +0800

    Add separate editions: commnity and enterprise

--- build/deps.cmd
+++ build/deps.cmd
@@ -17,15 +17,21 @@ CMD /c "npm install --no-audit"
 
 CD "%cur_dir%"
 
-if defined ZTM_VERSION (
-set VERSION=%ZTM_VERSION%
+if exist "hub\cluster.js" (
+    set EDITION=Enterprise
 ) else (
-FOR /f %%i IN ('git describe --abbrev^=0 --tags') DO SET VERSION=%%i
+    set EDITION=Community
+)
+
+if defined ZTM_VERSION (
+    set VERSION=%ZTM_VERSION%
+) else (
+    FOR /f %%i IN ('git describe --abbrev^=0 --tags') DO SET VERSION=%%i
 )
 
 FOR /f %%i IN ('git log -1 --format^=%%H') DO SET COMMIT=%%i
 FOR /f "eol= tokens=* delims=" %%i IN ('"git log -1 --format=%%cD"') DO SET COMMIT_DATE=%%i
 
-ECHO {"tag":"%VERSION%","commit":"%COMMIT%","date":"%OMMIT_DATE%"} > cli\version.json
+ECHO {"edition":"%EDITION%","tag":"%VERSION%","commit":"%COMMIT%","date":"%COMMIT_DATE%"} > cli\version.json
 COPY cli\version.json hub\version.json
 COPY cli\version.json agent\version.json
