99c0df3e24cf62b3983423758b78c965ac9f9291
DongChen Lin <393227948@qq.com>
2025-04-09 17:57:18 +0800

    upgrade tauri

--- gui/vite.config.js
+++ gui/vite.config.js
@@ -1,14 +1,12 @@
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { internalIpV4 } from "internal-ip";
-// import monacoEditorPlugin from 'vite-plugin-monaco-editor';
 import { resolve } from 'path';
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 
 const mobile = !!/android|ios/.exec(process.env.TAURI_ENV_PLATFORM);
+const host = process.env.TAURI_DEV_HOST;
 
-//target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
 export default defineConfig(async (config) => {
 	return {
 		clearScreen: false,
@@ -18,11 +16,11 @@ export default defineConfig(async (config) => {
 		server: {
 			port: 1420,
 			strictPort: true,
-			host: mobile ? "0.0.0.0" : false,
+			host: host || false,
 			hmr: mobile
 				? {
 						protocol: "ws",
-						host: await internalIpV4(),
+						host: host,
 						port: 1421,
 					}
 				: undefined,
@@ -41,7 +39,7 @@ export default defineConfig(async (config) => {
 			vue({reactivityTransform: true}),
 			createSvgIconsPlugin({
 				iconDirs: [resolve(process.cwd(), './src/assets/svg')],
-				symbolId: 'svg-[name]', // 自定义 symbolId 模板
+				symbolId: 'svg-[name]',
 			}),
 		],
 		resolve: {
