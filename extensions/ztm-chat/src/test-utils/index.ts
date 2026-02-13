4acee4b51c37ebcce7d818fcaa619358b03cbb54
Lin Yang <reaver@flomesh.io>
2026-02-14 01:28:48 +0800

    test(ztm-chat): add test infrastructure
    - Create src/test-utils/fixtures.ts with reusable test data
      - Config fixtures (testConfig, testConfigWithGroups, etc.)
      - User/peer fixtures (testUsers, testPeers)
      - Message fixtures (testMessages, unicodeMessage, longMessage)
      - Chat fixtures (testChats, testGroupChats)
      - Factory functions (createMessage, createChat, createUser, etc.)
    
    - Create src/test-utils/mocks.ts with reusable mock implementations
      - Logger mocks (createMockLogger, createMockLoggerFns)
      - Fetch mocks (createMockFetch)
      - API client mocks (createMockApiClient, createMockApiClientWithMessages)
      - Store mocks (createMockMessageStateStore, createMockPairingStore)
      - Mock shortcuts (mockSuccess, mockFailure, mockResolved)
    
    - Create src/test-utils/helpers.ts with utility functions
      - Timer helpers (advanceTimers, waitFor)
      - Cleanup helpers (createBeforeEach, createAfterEach)
      - Assertion helpers (expectToThrow, expectSuccess, expectFailure)
      - Async helpers (flushPromises, delay, nextTick)
      - Config helpers (minimalConfig, failingConfig, createTestClientConfig)
    
    - Create src/test-utils/index.ts as central export

---
+++ extensions/ztm-chat/src/test-utils/index.ts
@@ -0,0 +1,11 @@
+// Test Utilities - Central export for all test utilities
+// Provides fixtures, mocks, and helpers for ZTM Chat tests
+
+// Re-export fixtures
+export * from "./fixtures.js";
+
+// Re-export mocks
+export * from "./mocks.js";
+
+// Re-export helpers
+export * from "./helpers.js";
